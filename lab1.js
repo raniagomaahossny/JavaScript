180b76de02d088ca26f4e6f6b1b9ce7594770d38
rania <117826682+raniagomaahossny@users.noreply.github.com>
2023-01-05 14:52:14 -0800

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,35 @@
+const arrlist = {
+
+    [
+        {
+    firstName: 'Ahmed',
+    lastName: 'Ali',
+    dateOfBirth: '1995-10-10',
+    address: 'Alexadria, Egypt'
+  },
+  {
+    firstName: 'Hossam',
+    lastName: 'Mohamed',
+    dateOfBirth: '1980-05-10',
+    address: 'Nasr City, Cairo, Egypt'
+  },
+  {
+    firstName: 'John',
+    lastName: 'James',
+    dateOfBirth: '1975-03-05',
+    address: 'Nasr street, Nasr City, Cairo, Egypt'
+  },
+  {
+    firstName: 'Tarek',
+    lastName: 'Hassan',
+    dateOfBirth: '1999-12-03',
+    address: '15, street name, district, city, country'
+  },
+
+    ]
+    const myobj ={}:
+    for (i=0, i < arrlist, length , i++)
+    {
+        myobj [i]=arrlist[i]:
+    }
+}
